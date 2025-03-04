2ca20373c1f3d8b26c9f2aaff531b2274c634cd9
vasuki117psr <22cs117@psr.edu.in>
2025-03-04 09:25:40 +0530

    jobModel.js

--- backend/models/jobModel.js
+++ backend/models/jobModel.js
@@ -1,18 +1,16 @@
 const mongoose = require('mongoose');
-
-// Define the schema for job posts
 const jobSchema = mongoose.Schema({
-  jobTitle: { type: String, required: true },     // Job title (required)
-  ageLimit: { type: Number, required: true },     // Age limit (required)
-  location: { type: String, required: true },     // Location (required)
-  salary: { type: Number, required: true },       // Salary (required)
-  qualification: { type: String, required: true },// Qualification (required)
-  applyLink: { type: String, required: true },    // Apply link (required)
-  description: { type: String, required: true }   // Job description (required)
+  jobTitle: { type: String, required: true },     
+  ageLimit: { type: Number, required: true },     
+  location: { type: String, required: true },     
+  salary: { type: Number, required: true },       
+  qualification: { type: String, required: true }
+  applyLink: { type: String, required: true },    
+  description: { type: String, required: true }   
 });
 
-// Create the Job model using the schema
+
 const JobModel = mongoose.model('Job', jobSchema);  
 
-// Export the Job model
+
 module.exports = JobModel;
