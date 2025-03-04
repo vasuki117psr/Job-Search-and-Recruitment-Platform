88785e4d5df68393b5140b2cc3738beba63260e3
vasuki117psr <22cs117@psr.edu.in>
2025-03-04 09:27:34 +0530

    app.js

--- backend/app.js
+++ backend/app.js
@@ -19,7 +19,6 @@ mongoose.connect('mongodb://localhost:27017/job')
 .catch(err=>console.log(err))
 var app = express();
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
@@ -37,13 +36,13 @@ app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
+
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
+
   res.status(err.status || 500);
   res.render('error');
 });
